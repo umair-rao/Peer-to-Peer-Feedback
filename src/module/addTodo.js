8c15c494eeb6a2ce32ce6b044935e1e5733ebc5f
umair-rao <umairmuqeem@gmail.com>
2022-11-08 17:18:10 +0500

    Code updated

--- src/module/addTodo.js
+++ src/module/addTodo.js
@@ -4,5 +4,4 @@ const addTodo = (todoList) => {
   localStorage.setItem('todoList', JSON.stringify(todoList));
   displayTodo(todoList[todoList.length - 1], todoList.length);
 };
-
 export default addTodo;
